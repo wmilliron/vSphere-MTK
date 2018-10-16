811133b1479624d0bab6519d31e2b31ec1f391da
Wes Milliron <33131957+wmilliron@users.noreply.github.com>
2018-10-16 10:31:55 -0400

    Added Parameter verification in set-ipinfo

--- vSphere-MTK/Public/Set-IPInfo.ps1
+++ vSphere-MTK/Public/Set-IPInfo.ps1
@@ -1,28 +1,37 @@
 function Set-IPInfo{
     [cmdletbinding()]
     param (
-     [Parameter(ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)][String[]]$ComputerName = $env:computername,
-     [Parameter()][String[]]$ConfigurationPath
+        [Parameter(Mandatory=$True)][String]$VMName,
+        [Parameter()][String[]]$ConfigurationPath
     )
     begin{
         try{
-            Get-VIServer -erroraction stop
+            Get-VIServer -ErrorAction stop
         }
         catch{
-            connect-viserver
+            Connect-VIServer
         }
     }
     process{
         #Verify VM Exists
-        $Exists = Get-Vm -name $ComputerName -ErrorAction SilentlyContinue  
+        $Exists = Get-Vm -name $VMName -ErrorAction SilentlyContinue  
         if ($Exists){  
-            Write-Host "$ComputerName found."
+            Write-Host "$VMName found."
         }  
         else {  
-            Write-Host "VM named $ComputerName does not exist. Exiting..." -ForegroundColor Red
+            Write-Host "VM named $VMName does not exist. Exiting..." -ForegroundColor Red
             Return
         }
         
+        if (Test-Path $ConfigurationPath){
+            $IPData = Import-Csv -Path $ConfigurationPath
+        }
+        else{
+            Write-Error -Message "$ConfigurationPath cannot be reached. Exiting."
+            Return
+        }
+
+
     }
     end{}
 }
