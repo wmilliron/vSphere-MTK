cd5292de82a2406b5be2b10797d9b35092977442
Milliron <Wesley.Milliron@gianteagle.com>
2018-11-08 15:44:14 -0500

    further debugging of returned array

--- vSphere-MTK/Public/Set-IPInfo.ps1
+++ vSphere-MTK/Public/Set-IPInfo.ps1
@@ -7,11 +7,12 @@ function Set-IPInfo{
     begin{
         Import-Module VMware.PowerCLI
         #Ensure connection to vSphere
-        try{
-            Get-VIServer -ErrorAction stop
+        if ($global:DefaultVIServers.IsConnected -eq $True){
+            $VCName = $global:DefaultVIServers.Name
+            Write-Host "Already connected to $VCName. Continuing..."
         }
-        catch{
-            Connect-VIServer -Message "Please connect to vSphere.."
+        else {
+            Connect-VIServer
         }
         #Verify VM Exists
         $Exists = Get-Vm -name $VMName -ErrorAction SilentlyContinue
@@ -41,6 +42,9 @@ function Set-IPInfo{
         }
         #Harvests the Network Adapter data from the VM
         $cred = Get-Credential -Message "Please enter credentials with administrative access to the virtual machine's operating system."
+        $code = @'
+            Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object{$_.IPEnabled -eq $true} | Select-Object * 
+'@
         $Invoke = @{
             VM = "$VMName"
             ScriptType = 'PowerShell'
@@ -48,22 +52,24 @@ function Set-IPInfo{
             GuestUser = $cred.UserName
             GuestPassword = $cred.Password
         }
-        try{
-            $code = @'
-            Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object{$_.IPEnabled -eq $true}
-'@
 
-            $Adapters = Invoke-VMScriptPlus @Invoke -ErrorAction Stop 
-        }
-        catch{
-            Write-Error -Message "Unable to collect the NIC information from the VM"
-            return
-        }
+            $Adapters = (Invoke-VMScriptPlus @Invoke -ErrorAction Stop).ScriptOutput
+            $nl = [System.Environment]::NewLine
+            $Adapters = ($Adapters).Trim()
+            $Adapters = ($Adapters -Split("$nl$nl"))
+            $OSNICCount = $Adapters.count
+
+        #The below values are used for debugging
+        write-host "Config file NICCount is $NICCount"
+        write-host "Vsphere adapter count is $AdapterCount"
+        write-host "OS NIC count is $OSNICCount"
+
         #Compares the extracted adapter count to the VM-NIC count before proceeding.
         if ($NICCount -ne $Adapters.count){
             Write-Error -Message "The number of adapters required by the provided configuration file does not match the number of adapters queried from inside the Windows vm."
             return
         }
+        
         #Attempts to set IP configuration
         try{
             for ($i=0; $i -lt $Adapters.count; $i++){
@@ -72,10 +78,11 @@ function Set-IPInfo{
                 $gateway = ($IPData[$i].Gateway)
                 $dns = ($IPData[$i].DNSServers)
                 $code = @"
-                $Adapters[$i].EnableStatic($ip,$subnet)
-                $Adapters[$i].SetGateways($gateway)
-                $Adapters[$i].SetDNSServerSearchOrder($dns)
-                $Adapters[$i].SetDynamicDNSRegistration("TRUE")
+                $adapter = Get-WmiObject Win32NetworkAdapterConfiguration | Where-Object{$_.InterfaceIndex -eq $Adapters[$i].Index} 
+                $adapter.EnableStatic($ip,$subnet)
+                $adapter.SetGateways($gateway)
+                $adapter.SetDNSServerSearchOrder($dns)
+                $adapter.SetDynamicDNSRegistration("TRUE")
 "@
                 Invoke-VMScriptPlus @Invoke -ErrorAction Stop
             }
